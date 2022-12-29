f5573982cf88874558d33b82d5ab9135546da88f
Jonathan Xavier Ribeiro <jonathan.ribeiro@infracommerce.com.br>
2022-12-28 21:28:49 -0300

    adicionando banco de dados mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://apinest:123456@localhost:27017/admin'),
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TaskSchema } from './schemas/task.schema';
 import { TaskService } from './shared/task.service/task.service';
 import { TasksController } from './tasks.controller';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{ name: 'Task', schema: TaskSchema}])
+    ],
     controllers: [TasksController],
     providers: [TaskService]
 })
